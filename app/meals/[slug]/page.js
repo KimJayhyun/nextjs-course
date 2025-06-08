5ef81b92593d66f8b1ef5882babb986d9e90debe
KimJayhyun <dnlwjem000@gmail.com>
2025-06-08 19:01:07 +0900

    103. Header 리팩토링

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,7 @@
+export default function MealDetailsPage({ params }) {
+  return (
+    <>
+      <p>You are in {params.slug}</p>
+    </>
+  );
+}
---
+++ app/meals/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function MealsPage() {
+  return (
+    <>
+      <p>
+        <Link href="/">Go To Main Page</Link>
+      </p>
+    </>
+  );
+}
